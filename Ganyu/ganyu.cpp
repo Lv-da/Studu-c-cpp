ff6d74239ad83c3111d694a8d7e8eead2e00a51b
Lv_da <lvda62773@gmail.com>
2022-05-13 19:46:18 +0800

    Create ganyu.cpp

---
+++ Ganyu/ganyu.cpp
@@ -0,0 +1,13 @@
+#include "Ganyu.h"
+//using namespace GANYU;
+using namespace std;
+
+namespace GANYU{
+	void testUsed(){
+		cout<<"This is a test text"<<endl;
+	}
+	void testUsed2(){
+		cout<<"This is a test text"<<endl;
+		cout<<"It is worked!"<<endl;
+	}
+}
